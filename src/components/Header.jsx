f4b8c34fadfe24b6da3e14701dabfede51cc1aa6
EmilyM <155667566+EmilyMenken@user.noreply.github.com>
2025-10-05 23:28:36 -0700

    page finished

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import globe from '../assets/globe.png';
+
+
+export default function Header() {
+    return (
+        <header>
+            <img src={globe} alt="globe icon" />
+            <h1>My travel journal!</h1>
+        </header>
+    )
+}
